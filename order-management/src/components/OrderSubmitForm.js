5225b94dfa50e58505460e00e0c5dcdabefabbd5
lpstamenov <lpstamenov@gmail.com>
2021-09-16 12:19:51 +0300

    Make three fields

--- order-management/src/components/OrderSubmitForm.js
+++ order-management/src/components/OrderSubmitForm.js
@@ -3,48 +3,96 @@ import TextClass from "./TextClass";
 import Error from "./Error"
 
 class OrderSubmitForm extends React.Component {
-    state = {
-        email: "",
-        phone: "",
-        emailError: "",
-        isValid: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            phone: "",
+            nameError: "",
+            emailError: "",
+            phoneError: "",
+            isValid: false,
 
+        }
+        this.nameHandler = this.nameHandler.bind(this);
+        this.emailHandler = this.emailHandler.bind(this);
+        this.phoneHandler = this.phoneHandler.bind(this);
     }
 
 
     validate = () => {
+        this.setState({ isValid: true });
+
+        let nameError = "";
         let emailError = "";
-        let passwordError = "";
+        let phoneError = "";
+
+        if (!/^[A-Za-z ]+$/.test(this.state.name)) {
+            nameError = "invalid name";
+        }
+
         if (!this.state.email.includes('@')) {
             emailError = "invalid email";
         }
 
-        if (emailError) {
-            this.setState({ [this.state.emailError]: emailError, [this.state.isValid]: false });
+        var phoneValidationForm = /^\0?([0-9]{9})\)$/;
+        if (!this.state.phone.match(phoneValidationForm)) {
+            phoneError = "invalid phone number";
         }
-        this.setState({ [this.state.isValid]: true });
+
+        if (nameError) {
+            this.setState({ nameError: nameError, isValid: false });
+        }
+        if (emailError) {
+            this.setState({ emailError: emailError, isValid: false });
+        }
+        if (phoneError) {
+            this.setState({ phoneError: phoneError, isValid: false });
+        }
+
 
     }
 
-    onChangeEmail = event => {
-        this.setState({ [this.state.email]: event.target.value })
+    nameHandler = arg => {
+        this.setState({
+            name: arg
+        })
+    }
+
+    emailHandler = arg => {
+        this.setState({
+            email: arg
+        })
+    }
+
+    phoneHandler = arg => {
+        this.setState({
+            phone: arg
+        })
     }
 
     handleSubmit = event => {
+       /* this.validate();
         if (!this.state.isValid) {
+            console.log("wrong");
+            alert("Wrong input data. Please re-enter the data!");
             return;
-        }
+        }*/
         event.preventDefault();
         console.log(this.state);
-
     };
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
 
-                <TextClass /*value={this.state.email}*/ fieldName={"email"} onChange={this.onChangeEmail} />
-                <Error props={"invalid email"} />
+                <TextClass value={this.state.name} fieldName={"name"} handler={this.nameHandler.bind(this)} />
+                <Error props={this.state.nameError} />
+                <TextClass value={this.state.email} fieldName={"email"} handler={this.emailHandler.bind(this)} />
+                <Error props={this.state.emailError} />
+                <TextClass value={this.state.phone} fieldName={"phone"} handler={this.phoneHandler.bind(this)} />
+                <Error props={this.state.phoneError} />
 
                 <input type="submit" />
             </form>
--- order-management/src/components/TextClass.js
+++ order-management/src/components/TextClass.js
@@ -1,41 +1,22 @@
 import React from 'react';
 
-function TextClass({/*value,*/ fieldName, onChange}) {
-
-    //function changeHandler = event => {
-    // let valueError = "";
-    // this.setState({[this.isValid]: this.validate(event.target.value)});
-    // if (this.isValid) {
-    //     this.setState({ ...this.state, [event.target.name]: event.target.value })
-    //     this.props.parentCallback(this.value);
-    //     event.preventDefault();
-    // }
-    // else {
-    //     valueError = "invalid"
-    // }
-    // this.setState({ valueError });
-
-    // }
-    var newValue="";
-    var handleChange = (event) => {
-        newValue = event.target.value;
-        onChange(newValue);
-        //value=newValue;
-    }
+function TextClass({value, fieldName, handler }) {
 
     return (
         <label>
 
             <input
                 type="text"
-                //value={value}
-                onChange={handleChange}
+                defaultValue={value}
+                onChange={(event) => {
+                    const newValue = event.target.value;
+                    handler({ newValue })
+                }}
                 name={fieldName}
-            //placeholder="${fieldName}"
+
             />
-        </label>//,
-        //{newValue}
+        </label>
     )
 
 }
-export default TextClass  /*${fieldName}*/
+export default TextClass
