7caec000984b148ca6ba965d6883434b43cfa41a
Radina <r_nuneva@abv.bg>
2021-09-17 13:36:03 +0300

    Add function for checkboxes inside OrderSubmitForm

---
+++ order-management/src/components/CheckBox.js
@@ -0,0 +1,36 @@
+// import React from 'react';
+// //
+// function CheckBox(item, handler) {
+//     return (
+//         <label>
+//             <input type="checkbox"
+//                    onChange={handler(item.name)}
+//                    selected={this.state.selected.includes(item.name)}
+//             />
+//             { item.name }
+//         </label>
+//     )
+// //     return (
+// //         // <label>
+// //         //     <input
+// //         //         type="checkbox"
+// //         //         checked={checkbox}
+// //         //         // value={checkbox}
+// //         //         name={fieldName}
+// //         //         onChange={(event) => {
+// //         //             const newValue = event.target.value
+// //         //             handler(newValue)
+// //         //         }}
+// //         //     />
+// //         //     {fieldName}
+// //         // </label>
+// //         <label>
+// //             <input type="checkbox"
+// //                 onChange={() => this.onChange(name)}
+// //                 selected={this.state.selected.includes(name)}
+// //             />
+// //             <span>{ name }</span>
+// //         </label>
+// //     )
+// }
+// export default CheckBox
